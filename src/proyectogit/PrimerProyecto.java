1ea1d54affde5661c43efb4841e9623d14b8aa81
Alvaro Barcena <alvarobarcena27@gmail.com>
2022-11-30 13:42:26 +0100

    Primer commit (Root Commit)

---
+++ src/proyectogit/PrimerProyecto.java
@@ -0,0 +1,13 @@
+package proyectogit;
+
+public class PrimerProyecto {
+
+	public static void main(String[] args) {
+		
+		String saludo = "Hola Mundo";
+		
+		System.out.println(saludo);
+
+	}
+
+}
